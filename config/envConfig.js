05a17dc75e18b8f2198d9a6dd2b62f3201302221
KumarMohit85 <mohit.official.one@gmail.com>
2025-07-28 12:30:35 +0530

    changed .env settings for deployment

--- config/envConfig.js
+++ config/envConfig.js
@@ -3,5 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 
-export const ITICK_WS_URL = process.env.ITICK_WS_URL;
-export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN;
+export const ITICK_FOREX_WS_URL = process.env.ITICK_FOREX_WS_URL || "wss://api.itick.org/fws";
+export const ITICK_CRYPTO_WS_URL = process.env.ITICK_CRYPTO_WS_URL || "wss://api.itick.org/cws";
+export const ITICK_INDICES_WS_URL = process.env.ITICK_INDICES_WS_URL || "wss://api.itick.org/iws";
+export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN || "4e7da788657d4753a23ff72463f87d6b7508990e1b974eb4a4b8516bd596ded8"
