345235f768e7265e539e5366ffda84883ad45b74
KumarMohit85 <mohit.official.one@gmail.com>
2025-07-29 11:49:22 +0530

    changed api_key to created at 29-07

--- config/envConfig.js
+++ config/envConfig.js
@@ -6,4 +6,4 @@ dotenv.config();
 export const ITICK_FOREX_WS_URL = process.env.ITICK_FOREX_WS_URL || "wss://api.itick.org/fws";
 export const ITICK_CRYPTO_WS_URL = process.env.ITICK_CRYPTO_WS_URL || "wss://api.itick.org/cws";
 export const ITICK_INDICES_WS_URL = process.env.ITICK_INDICES_WS_URL || "wss://api.itick.org/iws";
-export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN || "e49bd6f88e1f4b21a8aee89bc6764e31d3cb7660c336480894fa3440c6bc66b2"
+export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN || "1b32e677c3cb46da878dff1a7ecdb9ae46d3043991ff48d2b92a74272b4e17c8"
