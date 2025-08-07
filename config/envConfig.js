0c9c6ddee263394fd51a8919990d9ae213ae87c1
KumarMohit85 <mohit.official.one@gmail.com>
2025-08-07 11:06:37 +0530

    changed api key to free plan

--- config/envConfig.js
+++ config/envConfig.js
@@ -6,4 +6,4 @@ dotenv.config();
 export const ITICK_FOREX_WS_URL = process.env.ITICK_FOREX_WS_URL || "wss://api.itick.org/fws";
 export const ITICK_CRYPTO_WS_URL = process.env.ITICK_CRYPTO_WS_URL || "wss://api.itick.org/cws";
 export const ITICK_INDICES_WS_URL = process.env.ITICK_INDICES_WS_URL || "wss://api.itick.org/iws";
-export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN || "4b13a6174f5040ea9f29d041052352af39c22525e5d347198a8611c07629f211"
+export const ITICK_WS_AUTH_TOKEN = process.env.ITICK_WS_AUTH_TOKEN || "a8da4be1aab640ffa229080adb141ea6f0c47d22ce7e4a349ac6cfffd1de825b"
