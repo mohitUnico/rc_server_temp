5371a05f24d88d8f68af8f84fadce6666e13153a
KumarMohit85 <mohit.21ug2013@iiitranchi.ac.in>
2025-11-24 12:59:24 +0530

    replacing one symbol to XAUUSD

--- services/forexSubscriptionService.js
+++ services/forexSubscriptionService.js
@@ -17,7 +17,7 @@ export class ForexSubscriptionService {
         // TEMPORARY: Limit to 3 symbols for testing
         // TODO: Revert this change to use all symbols
         // Original code: this.allSymbols = getAllForexSymbols();
-        this.allSymbols = ['EURUSD', 'GBPUSD', 'USDJPY']; // Only 3 major pairs for testing
+        this.allSymbols = ['XAUUSD', 'GBPUSD', 'USDJPY']; // Only 3 symbols for testing (including XAUUSD)
 
         // Get all 80 forex symbols from configuration (COMMENTED OUT - TEMPORARY)
         // this.allSymbols = getAllForexSymbols();
